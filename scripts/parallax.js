28c308276b04deb0ba10d328a90b64ede7b36722
Amaka Anikwe <amakaanikwe@gmail.com>
2019-05-01 08:00:45 -0400

    Updates

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,4 +1,4 @@
-/*!
+ /*!
  * parallax.js v1.5.0 (http://pixelcog.github.io/parallax.js/)
  * @copyright 2016 PixelCog, Inc.
  * @license MIT (https://github.com/pixelcog/parallax.js/blob/master/LICENSE)
@@ -405,8 +405,8 @@
 
   // Parallax Data-API
 
-  $( function () { 
-    $('[data-parallax="scroll"]').parallax(); 
+  $( function () {
+    $('[data-parallax="scroll"]').parallax();
   });
 
 }(jQuery, window, document));

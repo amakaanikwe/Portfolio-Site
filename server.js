13c086aa423589c55f5f21fdd217a977a0bfe376
Amaka Anikwe <amakaanikwe@gmail.com>
2019-05-23 06:30:57 -0400

    Public Folder

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const path = require("path");
 
 const PORT = 8080;
 
+// Data Parsing
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+app.post("/email", (req, res) => {
+  res.json({ message: "Message received!!!" });
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });

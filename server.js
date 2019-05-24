a83d227196ff2cc9ca6323c0bbd8d32ec22ae1fe
Amaka Anikwe <amakaanikwe@gmail.com>
2019-05-23 20:19:09 -0400

    Working Contact Form

--- server.js
+++ server.js
@@ -1,24 +1,59 @@
 const express = require("express");
 const log = console.log;
 const app = express();
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 const path = require("path");
 
 const PORT = 8080;
 
 // Data Parsing
-app.use(
-  express.urlencoded({
-    extended: false
-  })
-);
-app.use(express.json());
+// app.use(
+//   express.urlencoded({
+//     extended: false
+//   })
+// );
+// app.use(express.json());
 
-app.post("/email", (req, res) => {
-  res.json({ message: "Message received!!!" });
-});
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
+
+//POST route from contact form
+app.post('/contact', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: process.env.GMAIL_USER,
+    subject: 'New message from contact form',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-failure');
+    }
+    else {
+      res.render('contact-success');
+    }
+  });
+});
+
+// app.post("/email", (req, res) => {
+//   res.json({ message: "Message received!!!" });
+// });
+
+
 
 app.listen(PORT, () => log("Server is running on PORT,", 8080));
